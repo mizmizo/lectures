2f4ea45acdbc2d52e87a856d381cf758c408cf5f
mizmizo <mizohana@jsk.imi.i.u-tokyo.ac.jp>
2016-05-14 02:11:11 +0900

    create Q2 to Q7 answer

--- DL/chap3/homework.py
+++ DL/chap3/homework.py
@@ -13,4 +13,20 @@ def Q4(M, N, x0, x1, y0, y1):
     return cut.sum()
 
 def Q5_1(x):
+    one_hot = np.zeros((len(x), x.max() + 1))
+    one_hot[np.arange(len(x)), x] = 1
+    return one_hot
 
+def Q5_2(x):
+    return np.where(x == 1)[1]
+
+
+def Q6(m, n):
+    a = np.empty((0,m), int)
+    b = np.arange(1,m + 1)
+    c = np.arange(1,n + 1)
+    return np.array([np.append(a, np.array([b*i])) for i in c])
+
+def Q7(x):
+    y = x[1:len(x)]
+    return y - x[:len(x) - 1]
