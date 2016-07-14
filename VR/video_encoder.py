32269cf4ee4b828a2f705fbd62254c06a8042a6e
mizmizo <mizohana@jsk.imi.i.u-tokyo.ac.jp>
2016-07-14 17:11:07 +0900

    encode video into numpy.array

--- VR/video_encoder.py
+++ VR/video_encoder.py
@@ -19,13 +19,36 @@ def encode():
     if (len(frame.shape) == 2):
         print "input BGR movie!"
         quit()
+    frame = cv2.resize(frame, (160, 90))
+    cv2.imshow('frame', frame)
 
+    out_data = frame.reshape(1, 1, frame.shape[0] * frame.shape[1] * frame.shape[2])[0]
+    cnt = 0
+    init_flag = False
     while(cap.isOpened()):
         ret, frame = cap.read()
-        cv2.imshow('frame', frame)
+        if ret:
+            frame = cv2.resize(frame, (160, 90))
+            cv2.imshow('frame', frame)
+            if init_flag:
+                out_data = frame.reshape(1, 1, frame.shape[0] * frame.shape[1] * frame.shape[2])[0]
+                init_flag = False
+            else:
+                out_data = np.concatenate((out_data,frame.reshape(1, 1, frame.shape[0] * frame.shape[1] * frame.shape[2])[0]),axis=0)
+        else:
+            break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        if (len(out_data) > 1800):
+            print "save data %d: " % cnt + str(out_data.shape)
+            out_name = argv[1][argv[1].rfind("/") + 1 : argv[1].rfind(".")] + "_" + str(cnt) + ".npy"
+            np.save(out_name, out_data)
+            cnt += 1
+            init_flag = True
 
+    print "save data %d: " % cnt + str(out_data.shape)
+    out_name = argv[1][argv[1].rfind("/") + 1 : argv[1].rfind(".")] + "_" + str(cnt) + ".npy"
+    np.save(out_name, out_data)
     cap.release()
     cv2.destroyAllWindows()
 
